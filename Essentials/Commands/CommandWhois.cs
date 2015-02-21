562901206cda5c1bac29a2c6b3ac132a8e36efec
Android <1156645485@qq.com>
2015-02-21 15:31:19 +0800

    支持游戏3.6.9.0版本

--- Essentials/Commands/CommandWhois.cs
+++ Essentials/Commands/CommandWhois.cs
@@ -25,10 +25,10 @@ namespace Commands
                 if (SteamPlayerlist.tryGetSteamPlayer(K, out p))
                 {
                     SteamPlayerID pi = p.SteamPlayerID;
-                    RocketChatManager.Say(b.CSteamID, "人物名称: " + pi.CharacterName + " | Steam名称: " + pi.SteamName + " | 存档ID: " + pi.Y + "-" + pi.x);
+                    RocketChatManager.Say(b.CSteamID, "人物名称: " + pi.CharacterName + " | Steam名称: " + pi.SteamName + " | 存档ID: " + pi.N + "-" + pi.d);
                     PlayerLife life = p.Player.PlayerLife;
                     RocketChatManager.Say(b.CSteamID, "死亡: " + life.Dead + " | 出血: " + life.Bleeding + " | 骨折: " + life.Broken + " | 冻伤: " + life.Freezing + " | 氧气: " + life.Breath + "%");
-                    RocketChatManager.Say(b.CSteamID, "生命: " + life.v + "% | 饱食: " + life.Hunger + "% | 含水: " + life.Thirst + "% | 健康: " + life.Infection + "% | 耐力: " + life.Stamina + "%");
+                    RocketChatManager.Say(b.CSteamID, "生命: " + life.a + "% | 饱食: " + life.Hunger + "% | 含水: " + life.Thirst + "% | 健康: " + life.Infection + "% | 耐力: " + life.Stamina + "%");
                 }
                 else
                 {
